ed6255518dcf07433f2419d92d09e8a262d23a7b
Algebraity <algebraity@tutamail.com>
2026-04-18 15:05:34 -0400

    tried to fix a few small things--plz look at changes :3

--- token.h
+++ token.h
@@ -1,8 +1,11 @@
+#include<stddef.h>
+
 typedef enum {
     TOK_IDENT, TOK_NUMBER, TOK_EQUALS, TOK_LBRACE, TOK_RBRACE,
     TOK_COMMAND,         // e.g. \det or \otimes
     TOK_AMP, TOK_DBLBACKSLASH,
     TOK_EOF,
+    TOK_NONE=0
     /* ... */
 } TokenKind;
 
