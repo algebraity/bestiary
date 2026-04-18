3dce3e20ac57f8eb06fe27a237805b5503ae4ac8
Mizufuwa <mizufuwa@noreply.git.keimai.space>
2026-04-18 12:37:11 -0600

    make lexer have something
    idk, i am tired. this probably implements most of it.

--- lexer.c
+++ lexer.c
@@ -2,6 +2,11 @@
 #include <stdbool.h>
 #include "token.h"
 
+typedef enum
+{
+	READ_M, SCAN_M, COMMAND_M
+} LexMode;
+
 int bstlPush(Token* tokens,Token value)
 {
 	char counter;
@@ -14,12 +19,70 @@ int bstlPush(Token* tokens,Token value)
 	return 1;
 }
 
-#define push bstlPush;
-
 Token* bstLex(char* string)
 {
 	Token* tokens;
 	tokens = malloc(sizeof(Token));
+	LexMode mode = SCAN_M;
+	TokenKind curtok;
+	TokenKind lastok;
+	int inder = 0;
+	int bufr = 0;
+	char* buf;
+	buf = calloc(50,sizeof(char));
+	int i;
+	char backslashes;
+	for(i = 0 ; i < sizeof(*string) ; i++)
+	{
+		lastok = curtok;
+		switch (string[i]) {
+			case '1': case '2': case '3': case '4': case '5':
+			case '6': case '7': case '8': case '9': case '0':
+				curtok = TOK_NUMBER;
+				break;
+			case '=':
+				curtok = TOK_EQUALS;
+				break;
+			case '}':
+				curtok = TOK_LBRACE;
+				break;
+			case '{':
+				curtok = TOK_RBRACE;
+				break;
+			case '\\':
+				backslashes++;
+				if(backslashes > 1)
+				{
+					backslashes = 0;
+					curtok = TOK_DBLBACKSLASH;
+				}
+				curtok = TOK_COMMAND;
+				break;
+			case '&':
+				curtok = TOK_AMP;
+				break;
+			case (char)0xFF:
+				/* usually 0xFF is end of file */
+				curtok = TOK_EOF;
+				break;
+			default:
+				if(curtok != TOK_COMMAND)
+					curtok = TOK_IDENT;
+				break;
+		}
+		if (curtok != lastok)
+		{
+			Token* v = (Token*)calloc(1,sizeof(Token));
+			v->kind = lastok;
+			v->text = buf;
+			v->line = 1;
+			v->col = bufr;
+			bstlPush(tokens,*v);
+			free(v);
+			bufr = 0;
+			buf = calloc(50,sizeof(char));
+		}
+	}
 	/* TODO: start lexer here */
 	return tokens;
 }
