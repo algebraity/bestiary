639ea5856bd3f12944c0a2c38464b52352df59ff
Algebraity <algebraity@tutamail.com>
2026-04-21 14:34:14 -0400

    fixed some things

--- lexer.h
+++ lexer.h
@@ -1,4 +1,9 @@
+#ifndef BESTIARY_LEXER_H
+#define BESTIARY_LEXER_H
+
 /* prototypes */
 #include "token.h"
-Token* bstLex(char*);
-/* end proto */
+Token* bstLex(char* string, size_t* out_len);   // To Mizu: fixed function prototype here
+/* end prototypes */
+
+#endif
